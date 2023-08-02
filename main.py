fe645cd234afbb13dc3705507a02fbd8d0144153
joecryptotoo <80373433+joecryptotoo@users.noreply.github.com>
2023-08-01 19:17:59 -0700

    added debugging

--- main.py
+++ main.py
@@ -17,19 +17,28 @@ slotId = int(arg.slot)
 it = int(arg.iterations)
 i2c = busio.I2C(board.SCL, board.SDA, frequency=_WAKE_CLK_FREQ)
 
-atecc = ATECC(i2c,address=int(arg.i2c_address,16),debug=False)
+atecc = ATECC(i2c,address=int(arg.i2c_address,16),debug=True)
 
 results = []
 
+# print the public key
+print("Public Key: ", atecc.gen_key(bytearray(64),slotId).hex())
+
 def sign(loop):
     first = time.perf_counter()
-    if arg.random:
+    if arg.random == 'True':
         data = secrets.token_bytes(32)
     
     else:
-        data = b'\x01\x02\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x30\x31\x32'
+        # data = b'\x01\x02\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x30\x31\x32'
+        # with 32 bytes of 0x00
+        data = b'\x00' * 32
+
+    print("Data: ", data.hex())
 
     sig = atecc.ecdsa_sign(slotId,data)
+
+    print("Signature: ", sig.hex())
 
     last = time.perf_counter()
     delta = (last - first )* 1000
@@ -49,4 +58,3 @@ def run():
 if __name__ == "__main__":
     print("ATECC Serial: ", atecc.serial_number)
     run()
-
